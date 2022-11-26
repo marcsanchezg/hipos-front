85d43e503ae7eeba755aa65c53bb69b79576a1c4
marcsanchezg <marcmsg98@gmail.com>
2022-11-26 17:39:22 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import logo from './logo.svg';
+import logo from './hiposlogo.png';
 import './App.css';
+import { useState } from "react";
+import axios from "axios";
+
+
 
 function App() {
+  const [text, setText] = useState("");
+  const handleChange = (event) => {
+    setText({
+      ...text,
+      [event.target.name]: event.target.value
+    });
+  }
+
+
+
+  const handleSubmit = async() => {
+    const formData = new FormData();
+    formData.append("text", text.text)
+    console.log(text);
+    try {
+      const response = await axios ({
+        method: 'POST',
+        url: 'https://hipos.marcsg.com/s',
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        data: {text: text}
+      });
+    } catch(error){
+      console.log(error)
+    }
+  }  
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hipos</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={handleSubmit}>
+        <textarea value={text.text} onChange={handleChange} placeholder="Write here the secret you want to share" autoFocus={true}>
+          
+        </textarea>
+        <button type="submit">Create</button>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reacteeeeee
         </a>
       </header>
     </div>
